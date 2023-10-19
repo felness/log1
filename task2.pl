4b0069f62edc315335efb6802ff118d81cdd1480
felness <Nmedvedev85@mail.ru>
2023-10-20 01:05:04 +0300

    result second  part

--- task2.pl
+++ task2.pl
@@ -2,5 +2,18 @@
 
 % The line below imports the data
 :- ['one.pl'].
+average_grade(Group, Average) :-
+    student(Group, _),
+    findall(Grade, grade(_, _, Grade), Grades),
+    length(Grades, NumGrades),
+    sum_list(Grades, Sum),
+    Average is Sum / NumGrades.
 
-group(X,L) :- findall(Z,student(X,Z),L).
+students_failed_exam(Subject, Students) :-
+    subject(Subject, _),
+    findall(Student, (student(_, Student), \+ grade(Student, Subject, 2)), Students).
+
+num_students_failed_exam(Group, NumStudents) :-
+    student(Group, _),
+    findall(Student, (student(Group, Student), grade(Student, _, 2)), FailedStudents),
+    length(FailedStudents, NumStudents).
